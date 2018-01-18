2938f53b8a62b4c900e1bb9e407d0822d4136de2
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-18 12:55:54 +0700

    2

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text+"ลงท้ายครับ"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
