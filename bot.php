64e6097c25cc1e5bcf87cab2ac72b619f9da743e
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-18 13:15:01 +0700

    ++

--- bot.php
+++ bot.php
@@ -17,10 +17,22 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text."ลงท้ายครับ"
-			];
+      $context = substr($text, 0, 1);
+      if($context == "P"){
+
+        $messages = [
+          'type' => 'text',
+          'text' => 'บวก'
+        ];
+      }
+      else if($context == "M"){
+
+        $messages = [
+          'type' => 'text',
+          'text' => 'คูณ'
+        ];
+      }
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
