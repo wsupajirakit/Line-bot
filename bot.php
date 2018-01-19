4e92173be717a1a0cf0bba7b1264d35b0577ee59
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-19 14:37:58 +0700

    111

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
       $context = substr($text, 0, 2);
 			$ftext = substr($text, 0, 1);
-			if(strtoupper($ftext) == "@"){
+			if(strtoupper($ftext) == "A"){
 
         $messages = [
           'type' => 'text',
