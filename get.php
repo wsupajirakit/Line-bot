875338136a3b5d5b156528089381e2672c51e316
wsupajirakit <wsupajirakit@gmail.com>
2018-01-22 23:14:53 +0700

    -

--- get.php
+++ get.php
@@ -1,11 +1,14 @@
 <?php
 echo '3333';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '56c7d7a131821a15a2f406ce300ea4eb']);
-$response = $bot->getProfile('Uea8f71000cb4673f4d4431dee2318edd');
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo $profile['displayName'];
-    echo $profile['pictureUrl'];
-    echo $profile['statusMessage'];
+
+$bot = new \LINE\LINEBot(new CurlHTTPClient('oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU='), [
+    'channelSecret' => '56c7d7a131821a15a2f406ce300ea4eb'
+]);
+
+$res = $bot->getProfile('Uea8f71000cb4673f4d4431dee2318edd');
+if ($res->isSucceeded()) {
+    $profile = $res->getJSONDecodedBody();
+    $displayName = $profile['displayName'];
+    $statusMessage = $profile['statusMessage'];
+    $pictureUrl = $profile['pictureUrl'];
 }
