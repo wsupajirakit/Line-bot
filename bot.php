63e8c94e7eb7ecee812b5be90029dd5d0275cb22
wsupajirakit <wsupajirakit@gmail.com>
2018-01-22 12:20:30 +0700

    11

--- bot.php
+++ bot.php
@@ -23,124 +23,124 @@ if (!is_null($events['events'])) {
       $context = substr($text, 0, 2);
 			$ftext = substr($text, 0, 1);
 
-      if(strtoupper($ftext) == "@"{
-        // $forwardtext = strstr($text, '-', true);
-        // $player = substr($forwardtext, 1);
-        //
-        // $money  = substr($text, (strpos($text, '-') ?: -1) + 1);
+      // if(strtoupper($ftext) == "P"){
+      //   $forwardtext = strstr($text, '-', true);
+      //   $player = substr($forwardtext, 1);
+      //
+      //   $money  = substr($text, (strpos($text, '-') ?: -1) + 1);
+      //
+      //   $messages = [
+      //     'type' => 'text',
+      //     'text' => 'แทงผู้เล่น : '.$player.' จำนวนเงิน'.'by'.$money
+      //   ];
+      // }
+
+			if($ftext == "@"){
+					$forwardtext = strstr($text, '+', true);
+					$id = substr($forwardtext, 1);
+					$money  = substr($text, (strpos($text, '+') ?: -1) + 1);
+
+					$uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Balance%20where%20balance_tks_userid='".$id."'%20;";
+			    $response = \Httpful\Request::get($uri)->send();
+			    // echo $response;
+			    $sum = $response->body->result[0]->balance_tks_balance;
+					$sum = $sum+$money;
+
+
+					$curl = curl_init();
+
+					curl_setopt_array($curl, array(
+					  CURLOPT_URL => "http://redfoxdev.com/vtiger/webservice.php",
+					  CURLOPT_RETURNTRANSFER => true,
+					  CURLOPT_ENCODING => "",
+					  CURLOPT_MAXREDIRS => 10,
+					  CURLOPT_TIMEOUT => 30,
+					  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					  CURLOPT_CUSTOMREQUEST => "POST",
+					  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\nupdate\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sessionName\"\r\n\r\n41fd14e15a617f672c0fd\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"element\"\r\n\r\n{\n            \"balanceno\": \"\",\n            \"balance_tks_userid\": \"001\",\n            \"balance_tks_balance\": \"$sum\",\n            \"assigned_user_id\": \"19x1\",\n            \"createdtime\": \"2018-01-19 05:16:44\",\n            \"modifiedtime\": \"2018-01-19 05:16:44\",\n            \"id\": \"47x531\"\n        }\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+					  CURLOPT_HTTPHEADER => array(
+					    "cache-control: no-cache",
+					    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+					    "postman-token: 533f06b3-0821-a7ea-5d31-36f0ce51e03f"
+					  ),
+					));
+
+					$response = curl_exec($curl);
+					$result = json_decode($response);
+
+					$err = curl_error($curl);
+
+					if ($err) {
+					  echo "cURL Error #:" . $err;
+					} else {
+					}
+
+
+					curl_close($curl);
+
 
         $messages = [
           'type' => 'text',
-          'text' => 'แทง'
+          'text' => 'ฝากเงินสำเร็จ คุณมียอดเงินคงเหลือ : '.$sum.' บาท'.'by'.$userID
         ];
       }
 
-			// else if($ftext == "@"){
-			// 		$forwardtext = strstr($text, '+', true);
-			// 		$id = substr($forwardtext, 1);
-			// 		$money  = substr($text, (strpos($text, '+') ?: -1) + 1);
-      //
-			// 		$uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Balance%20where%20balance_tks_userid='".$id."'%20;";
-			//     $response = \Httpful\Request::get($uri)->send();
-			//     // echo $response;
-			//     $sum = $response->body->result[0]->balance_tks_balance;
-			// 		$sum = $sum+$money;
-      //
-      //
-			// 		$curl = curl_init();
-      //
-			// 		curl_setopt_array($curl, array(
-			// 		  CURLOPT_URL => "http://redfoxdev.com/vtiger/webservice.php",
-			// 		  CURLOPT_RETURNTRANSFER => true,
-			// 		  CURLOPT_ENCODING => "",
-			// 		  CURLOPT_MAXREDIRS => 10,
-			// 		  CURLOPT_TIMEOUT => 30,
-			// 		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			// 		  CURLOPT_CUSTOMREQUEST => "POST",
-			// 		  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\nupdate\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sessionName\"\r\n\r\n41fd14e15a617f672c0fd\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"element\"\r\n\r\n{\n            \"balanceno\": \"\",\n            \"balance_tks_userid\": \"001\",\n            \"balance_tks_balance\": \"$sum\",\n            \"assigned_user_id\": \"19x1\",\n            \"createdtime\": \"2018-01-19 05:16:44\",\n            \"modifiedtime\": \"2018-01-19 05:16:44\",\n            \"id\": \"47x531\"\n        }\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
-			// 		  CURLOPT_HTTPHEADER => array(
-			// 		    "cache-control: no-cache",
-			// 		    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-			// 		    "postman-token: 533f06b3-0821-a7ea-5d31-36f0ce51e03f"
-			// 		  ),
-			// 		));
-      //
-			// 		$response = curl_exec($curl);
-			// 		$result = json_decode($response);
-      //
-			// 		$err = curl_error($curl);
-      //
-			// 		if ($err) {
-			// 		  echo "cURL Error #:" . $err;
-			// 		} else {
-			// 		}
-      //
-      //
-			// 		curl_close($curl);
-      //
-      //
-      //   $messages = [
-      //     'type' => 'text',
-      //     'text' => 'ฝากเงินสำเร็จ คุณมียอดเงินคงเหลือ : '.$sum.' บาท'.'by'.$userID
-      //   ];
-      // }
-      //
-      // else if(strtoupper($text) == "REGISTER"){
-      //
-      //
-      //   $messages = [
-      //     'type' => 'text',
-      //     'text' => $userID
-      //   ];
-      // }
-      //
-      // else if(strtoupper($context) == "PL"){
-      //
-      //   $forwardtext = strstr($text, '+', true);
-      //   $num1 = substr($forwardtext, 3);
-      //   $num2  = substr($text, (strpos($text, '+') ?: -1) + 1);
-      //   $sumall = $num1+$num2;
-      //   $messages = [
-      //     'type' => 'text',
-      //     'text' => $sumall
-      //   ];
-      // }
-      // else if(strtoupper($context) == "MU"){
-      //
-      //   $forwardtext = strstr($text, '*', true);
-      //   $num1 = substr($forwardtext, 3);
-      //   $num2  = substr($text, (strpos($text, '*') ?: -1) + 1);
-      //   $sumall = $num1*$num2;
-      //   $messages = [
-      //     'type' => 'text',
-      //     'text' => $sumall
-      //   ];
-      // }
-      // else if(strtoupper($context) == "MI"){
-      //
-      //   $forwardtext = strstr($text, '-', true);
-      //   $num1 = substr($forwardtext, 3);
-      //   $num2  = substr($text, (strpos($text, '-') ?: -1) + 1);
-      //   $sumall = $num1-$num2;
-      //   $messages = [
-      //     'type' => 'text',
-      //     'text' => $sumall
-      //   ];
-      // }
-      //
-      // else if(strtoupper($context) == "DI"){
-      //
-      //   $forwardtext = strstr($text, '/', true);
-      //   $num1 = substr($forwardtext, 3);
-      //   $num2  = substr($text, (strpos($text, '/') ?: -1) + 1);
-      //   $sumall = $num1/$num2;
-      //   $messages = [
-      //     'type' => 'text',
-      //     'text' => $sumall
-      //   ];
-      // }
+      else if(strtoupper($text) == "REGISTER"){
 
-      else if{
+
+        $messages = [
+          'type' => 'text',
+          'text' => $userID
+        ];
+      }
+
+      else if(strtoupper($context) == "PL"){
+
+        $forwardtext = strstr($text, '+', true);
+        $num1 = substr($forwardtext, 3);
+        $num2  = substr($text, (strpos($text, '+') ?: -1) + 1);
+        $sumall = $num1+$num2;
+        $messages = [
+          'type' => 'text',
+          'text' => $sumall
+        ];
+      }
+      else if(strtoupper($context) == "MU"){
+
+        $forwardtext = strstr($text, '*', true);
+        $num1 = substr($forwardtext, 3);
+        $num2  = substr($text, (strpos($text, '*') ?: -1) + 1);
+        $sumall = $num1*$num2;
+        $messages = [
+          'type' => 'text',
+          'text' => $sumall
+        ];
+      }
+      else if(strtoupper($context) == "MI"){
+
+        $forwardtext = strstr($text, '-', true);
+        $num1 = substr($forwardtext, 3);
+        $num2  = substr($text, (strpos($text, '-') ?: -1) + 1);
+        $sumall = $num1-$num2;
+        $messages = [
+          'type' => 'text',
+          'text' => $sumall
+        ];
+      }
+
+      else if(strtoupper($context) == "DI"){
+
+        $forwardtext = strstr($text, '/', true);
+        $num1 = substr($forwardtext, 3);
+        $num2  = substr($text, (strpos($text, '/') ?: -1) + 1);
+        $sumall = $num1/$num2;
+        $messages = [
+          'type' => 'text',
+          'text' => $sumall
+        ];
+      }
+
+      else {
 
         $messages = [
           'type' => 'text',
