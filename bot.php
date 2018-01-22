39f4ccc7ec772768e928db29612bb236ae994ec8
wsupajirakit <wsupajirakit@gmail.com>
2018-01-22 14:59:47 +0700

    asd

--- bot.php
+++ bot.php
@@ -156,38 +156,9 @@ if (!is_null($events['events'])) {
              $player = $response->body->result[$i]->cf_960;
              $newbalance = $balance - $bet;
 
-             $curl = curl_init();
-
-             curl_setopt_array($curl, array(
-               CURLOPT_URL => "http://redfoxdev.com/vtiger/webservice.php",
-               CURLOPT_RETURNTRANSFER => true,
-               CURLOPT_ENCODING => "",
-               CURLOPT_MAXREDIRS => 10,
-               CURLOPT_TIMEOUT => 30,
-               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-               CURLOPT_CUSTOMREQUEST => "POST",
-               CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\nupdate\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sessionName\"\r\n\r\n244bae35a6579977f668\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"element\"\r\n\r\n{\n            \"balanceno\": \"\",\n            \"balance_tks_userid\": \"$userID\",\n            \"balance_tks_balance\": \"$newbalance\",\n            \"assigned_user_id\": \"19x1\",\n            \"createdtime\": \"2018-01-22 04:44:00\",\n            \"modifiedtime\": \"2018-01-22 05:50:35\",\n
-                 \"cf_956\": \"$bet\",\n            \"cf_958\": \"$username\",\n            \"cf_960\": \"$player\",\n            \"id\": \"$vid\"\n        }\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"elementType\"\r\n\r\nBalance\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
-               CURLOPT_HTTPHEADER => array(
-                 "Cache-Control: no-cache",
-                 "Postman-Token: 8cf07109-175f-5368-08c6-63279568d118",
-                 "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
-               ),
-             ));
-
-             $response = curl_exec($curl);
-             $err = curl_error($curl);
-
-             curl_close($curl);
-
-             if ($err) {
-               echo "cURL Error #:" . $err;
-             } else {
-             }
 
 
-
-             $listname = $listname."\n ".$username."  -".$bet;
+             $listname = $listname."\n ".$username."  -".$bet."= + ".$newbalance.'บาท';
 
          }
 
