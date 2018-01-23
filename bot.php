398b6c346f597d7ce3c698827da310fbad3cfd59
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-23 15:49:36 +0700

    1234

--- bot.php
+++ bot.php
@@ -12,7 +12,21 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+
+    		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
+          $pid = $event['message']['packageId'];
+          $sid = $event['message']['stickerId'];
+
+          $messages = [
+            'type' => 'text',
+            // 'text' => 'แทงผู้เล่น'.$player.'จำนวน'.$money.'ชื่อผู้เล่น'.$username.'ยอดคงเหลือ'.$balance.'vid:'.$vid
+            'text' => $pid."/////".$sid
+          ];
+
+        }
+
+
+		      else if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
       $userID = $event['source']['userId'];
