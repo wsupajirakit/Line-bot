5b67bb0878e8e5ebe3afa633d53132f5352f0b5e
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-23 16:13:40 +0700

    1234

--- bot.php
+++ bot.php
@@ -2268,11 +2268,15 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		} 	else if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
 
+      $pid = $event['message']['packageId'];
+      $sid = $event['message']['stickerId'];
 			$replyToken = $event['replyToken'];
 			$messages = [
 				'type' => 'text',
-				'text' => 'sticker'
+				'text' => $pid."///".$sid
 			];
+
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
