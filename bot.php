d687bfe4103b34bf9afc2dcec58876611e451e36
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-23 15:52:04 +0700

    ++

--- bot.php
+++ bot.php
@@ -23,6 +23,24 @@ if (!is_null($events['events'])) {
             'text' => $pid."/////".$sid
           ];
 
+              $url = 'https://api.line.me/v2/bot/message/reply';
+              $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+              ];
+              $post = json_encode($data);
+              $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+              $ch = curl_init($url);
+              curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+              curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+              curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+              curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+              $result = curl_exec($ch);
+              curl_close($ch);
+
+              echo $result . "\r\n";
         }
 
 
