a0acb65d0586676630e71a88374bef684452bda6
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-23 16:11:59 +0700

    1234

--- bot.php
+++ bot.php
@@ -12,27 +12,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-
-		   if ($event['type'] == 'message' && $event['message']['type'] == 'text' ||  $event['message']['type'] == 'sticker') {
-
-         $typem = $event['message']['type'];
-
-         if($typem == "sticker"){
-           $pid = $event['message']['packageId'];
-           $sid = $event['message']['stickerId'];
-
-           $messages = [
-             'type' => 'text',
-             'text' => $pid."/////".$sid
-           ];
-         }
-
-          else if ($typem == "text") {
-
-
-
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-      $text = $event['message']['text'];
+			$text = $event['message']['text'];
       $userID = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
@@ -2284,8 +2266,34 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+		} 	else if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
+
+			$replyToken = $event['replyToken'];
+			$messages = [
+				'type' => 'text',
+				'text' => 'sticker'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		}
 	}
-  }
 }
 echo "OK";
