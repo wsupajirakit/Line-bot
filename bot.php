78cf6fae118460d0f7ff35bda82625da19a7ea47
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-24 14:58:21 +0700

    1234

--- bot.php
+++ bot.php
@@ -2247,14 +2247,14 @@ if (!is_null($events['events'])) {
 					$id = substr($forwardtext, 1);
 					$money  = substr($text, (strpos($text, '+') ?: -1) + 1);
 
-					$uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Balance%20where%20cf_958=%27".$id."%27%20;";
+					$uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Balance%20where%20id=%27".$id."%27%20;";
 			    $response = \Httpful\Request::get($uri)->send();
 			    // echo $response;
 			    $sum = $response->body->result[0]->balance_tks_balance;
 					$sum = $sum+$money;
 
 
-          $username = $response->body->result[0]->cf_958;
+          $usernamex = $response->body->result[0]->cf_958;
           $userIDx = $response->body->result[0]->balance_tks_userid;
           $vid = $response->body->result[0]->id;
 
@@ -2270,7 +2270,7 @@ if (!is_null($events['events'])) {
 					  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 					  CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"operation\"\r\n\r\nupdate\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sessionName\"\r\n\r\n244bae35a6579977f668\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"element\"\r\n\r\n{\n            \"balanceno\": \"\",\n            \"balance_tks_userid\": \"$userIDx\",\n            \"balance_tks_balance\": \"$sum\",\n            \"assigned_user_id\": \"19x1\",\n            \"createdtime\": \"2018-01-22 04:44:00\",\n            \"modifiedtime\": \"2018-01-22 05:50:35\",\n
-              \"cf_956\": \"\",\n            \"cf_958\": \"$username\",\n            \"cf_960\": \"\",\n            \"id\": \"$vid\"\n        }\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"elementType\"\r\n\r\nBalance\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+              \"cf_956\": \"\",\n            \"cf_958\": \"$usernamex\",\n            \"cf_960\": \"\",\n            \"id\": \"$vid\"\n        }\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"elementType\"\r\n\r\nBalance\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
             CURLOPT_HTTPHEADER => array(
               "Cache-Control: no-cache",
               "Postman-Token: 8cf07109-175f-5368-08c6-63279568d118",
@@ -2294,7 +2294,7 @@ if (!is_null($events['events'])) {
 
         $messages = [
           'type' => 'text',
-          'text' => 'ฝากเงินสำเร็จ'.$id.'มียอดเงินคงเหลือ : '.$sum
+          'text' => 'ฝากเงินสำเร็จ'.$usernamex.'มียอดเงินคงเหลือ : '.$sum
         ];
       }
 
