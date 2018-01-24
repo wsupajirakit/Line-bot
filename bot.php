6d93c310d3a86e4c613ee0d14dbb739d666ed4a2
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-24 13:33:45 +0700

    1234

--- bot.php
+++ bot.php
@@ -154,11 +154,45 @@ if (!is_null($events['events'])) {
                 ];
               }
             }else {
+
+              $dname= '';
+              $curl = curl_init();
+
+              curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.line.me/v2/bot/profile/".$userID,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                  "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
+                  "cache-control: no-cache",
+                  "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
+                ),
+              ));
+
+              $response = curl_exec($curl);
+              $err = curl_error($curl);
+
+              curl_close($curl);
+
+              if ($err) {
+                echo "cURL Error #:" . $err;
+              } else {
+              //   echo $response;
+              //
+              $data = json_decode($response,true);
+              $dname =  $data['displayName'];
+
               $messages = [
                 'type' => 'text',
                 // 'text' => 'แทงผู้เล่น'.$player.'จำนวน'.$money.'ชื่อผู้เล่น'.$username.'ยอดคงเหลือ'.$balance.'vid:'.$vid
-                'text' => 'คุณไม่ได้เป็นสมาชิก'
+                'text' => .$dname.'ไม่ได้เป็นสมาชิก'
               ];
+            }
+
             }
 
       }
