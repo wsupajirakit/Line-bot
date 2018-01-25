ccddf58224c0e66d906e67f6651f4a744dcd8f3b
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-25 20:16:05 +0700

    1234

--- bot.php
+++ bot.php
@@ -2405,39 +2405,6 @@ if (!is_null($events['events'])) {
       }
 			else if($ftext == "@"){
 
-        $dname= '';
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.line.me/v2/bot/group/".$groupID."/member/".$userID,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_HTTPHEADER => array(
-            "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
-            "cache-control: no-cache",
-            "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
-          ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-          echo "cURL Error #:" . $err;
-        } else {
-
-        $data = json_decode($response,true);
-        $dname =  $data['displayName'];
-        }
-
-
-
         $myid = substr($text,1);
 
         if(strcmp($myid,"id") == 0){
@@ -2470,10 +2437,12 @@ if (!is_null($events['events'])) {
 
           $uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Games%20Where%20id%20=%20'43x539';";
           $response = \Httpful\Request::get($uri)->send();
+
           $adminID = $response->body->result[0]->games_tks_password;
 
 
             if(strcmp($adminID,$userID) == 0){
+
                           $cont= '';
                           if (strpos($text, '+') !== false) {
                             $forwardtext = strstr($text, '+', true);
@@ -2544,7 +2513,7 @@ if (!is_null($events['events'])) {
 
                           $messages = [
                             'type' => 'text',
-                            'text' => 'ปรับยอดเงินสำเร็จ'.$dname.'มียอดเงินคงเหลือ : '.$sum
+                            'text' => 'ปรับยอดเงินสำเร็จ'.$usernamex.'มียอดเงินคงเหลือ : '.$sum
                           ];
 
                         } else {
@@ -2626,7 +2595,7 @@ if (!is_null($events['events'])) {
 
                                     $messages = [
                                       'type' => 'text',
-                                      'text' => 'สมัครสมาชิกสำเร็จ '.$dname.
+                                      'text' => 'สมัครสมาชิกสำเร็จ '.$dname
                                     ];
 
                                   }
