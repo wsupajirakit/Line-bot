6b518a29c50ab0478c40de9f12067b2e6a98c998
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-25 20:21:19 +0700

    1234

--- bot.php
+++ bot.php
@@ -2468,6 +2468,8 @@ if (!is_null($events['events'])) {
 
         } else {
 
+
+          $dname= '';
           $uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Games%20Where%20id%20=%20'43x539';";
           $response = \Httpful\Request::get($uri)->send();
 
@@ -2475,6 +2477,38 @@ if (!is_null($events['events'])) {
 
 
             if(strcmp($adminID,$userID) == 0){
+
+
+                                            $curl = curl_init();
+
+                                            curl_setopt_array($curl, array(
+                                              CURLOPT_URL => "https://api.line.me/v2/bot/group/".$groupID."/member/".$userID,
+                                              CURLOPT_RETURNTRANSFER => true,
+                                              CURLOPT_ENCODING => "",
+                                              CURLOPT_MAXREDIRS => 10,
+                                              CURLOPT_TIMEOUT => 30,
+                                              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                              CURLOPT_CUSTOMREQUEST => "GET",
+                                              CURLOPT_HTTPHEADER => array(
+                                                "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
+                                                "cache-control: no-cache",
+                                                "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
+                                              ),
+                                            ));
+
+                                            $response = curl_exec($curl);
+                                            $err = curl_error($curl);
+
+                                            curl_close($curl);
+
+                                            if ($err) {
+                                              echo "cURL Error #:" . $err;
+                                            } else {
+
+                                            $data = json_decode($response,true);
+                                            $dname =  $data['displayName'];
+                                            }
+
 
                           $cont= '';
                           if (strpos($text, '+') !== false) {
@@ -2505,8 +2539,6 @@ if (!is_null($events['events'])) {
                             }
 
 
-
-                                                          $dname= '';
                             $usernamex = $response->body->result[0]->cf_958;
                             $userIDx = $response->body->result[0]->balance_tks_userid;
                             $vid = $response->body->result[0]->id;
@@ -2540,35 +2572,6 @@ if (!is_null($events['events'])) {
                   					  echo "cURL Error #:" . $err;
                   					} else {
 
-                              $curl = curl_init();
-
-                              curl_setopt_array($curl, array(
-                                CURLOPT_URL => "https://api.line.me/v2/bot/group/".$groupID."/member/".$userID,
-                                CURLOPT_RETURNTRANSFER => true,
-                                CURLOPT_ENCODING => "",
-                                CURLOPT_MAXREDIRS => 10,
-                                CURLOPT_TIMEOUT => 30,
-                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                                CURLOPT_CUSTOMREQUEST => "GET",
-                                CURLOPT_HTTPHEADER => array(
-                                  "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
-                                  "cache-control: no-cache",
-                                  "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
-                                ),
-                              ));
-
-                              $response = curl_exec($curl);
-                              $err = curl_error($curl);
-
-                              curl_close($curl);
-
-                              if ($err) {
-                                echo "cURL Error #:" . $err;
-                              } else {
-
-                              $data = json_decode($response,true);
-                              $dname =  $data['displayName'];
-                              }
                   					}
 
 
