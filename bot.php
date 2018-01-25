761b0422c5be908499f274068f6f38e04c949d8a
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-25 20:17:55 +0700

    1234

--- bot.php
+++ bot.php
@@ -2409,6 +2409,39 @@ if (!is_null($events['events'])) {
 
         if(strcmp($myid,"id") == 0){
 
+          $dname= '';
+          $curl = curl_init();
+
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.line.me/v2/bot/group/".$groupID."/member/".$userID,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+              "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
+              "cache-control: no-cache",
+              "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
+            ),
+          ));
+
+          $response = curl_exec($curl);
+          $err = curl_error($curl);
+
+          curl_close($curl);
+
+          if ($err) {
+            echo "cURL Error #:" . $err;
+          } else {
+
+          $data = json_decode($response,true);
+          $dname =  $data['displayName'];
+          }
+
+
+
           $uri = "http://redfoxdev.com/vtiger/webservice.php?operation=query&sessionName=41fd14e15a617f672c0fd&query=select%20*%20from%20%20Balance%20where%20balance_tks_userid='".$userID."'%20;";
           $response = \Httpful\Request::get($uri)->send();
           // echo $response;
