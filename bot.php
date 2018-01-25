9b8ad598e9c5990b371f4410509e8d8bda85499d
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-25 20:19:32 +0700

    444

--- bot.php
+++ bot.php
@@ -2506,6 +2506,7 @@ if (!is_null($events['events'])) {
 
 
 
+                                                          $dname= '';
                             $usernamex = $response->body->result[0]->cf_958;
                             $userIDx = $response->body->result[0]->balance_tks_userid;
                             $vid = $response->body->result[0]->id;
@@ -2538,6 +2539,36 @@ if (!is_null($events['events'])) {
                   					if ($err) {
                   					  echo "cURL Error #:" . $err;
                   					} else {
+
+                              $curl = curl_init();
+
+                              curl_setopt_array($curl, array(
+                                CURLOPT_URL => "https://api.line.me/v2/bot/group/".$groupID."/member/".$userID,
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_ENCODING => "",
+                                CURLOPT_MAXREDIRS => 10,
+                                CURLOPT_TIMEOUT => 30,
+                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                CURLOPT_CUSTOMREQUEST => "GET",
+                                CURLOPT_HTTPHEADER => array(
+                                  "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
+                                  "cache-control: no-cache",
+                                  "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
+                                ),
+                              ));
+
+                              $response = curl_exec($curl);
+                              $err = curl_error($curl);
+
+                              curl_close($curl);
+
+                              if ($err) {
+                                echo "cURL Error #:" . $err;
+                              } else {
+
+                              $data = json_decode($response,true);
+                              $dname =  $data['displayName'];
+                              }
                   					}
 
 
@@ -2546,7 +2577,7 @@ if (!is_null($events['events'])) {
 
                           $messages = [
                             'type' => 'text',
-                            'text' => 'ปรับยอดเงินสำเร็จ'.$usernamex.'มียอดเงินคงเหลือ : '.$sum
+                            'text' => 'ปรับยอดเงินสำเร็จ'.$dname.'มียอดเงินคงเหลือ : '.$sum
                           ];
 
                         } else {
