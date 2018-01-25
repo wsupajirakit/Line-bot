5166d6f446c5bdaac5bab057b192a960a3da91a6
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-25 19:02:30 +0700

    1234

--- bot.php
+++ bot.php
@@ -1548,8 +1548,40 @@ if (!is_null($events['events'])) {
           $datax = json_decode($responsex,true);
 
           foreach($datax["result"] as $itemx) {
-              $username = $itemx['cf_958'];
+              $username = '';
               $userID = $itemx['balance_tks_userid'];
+
+              $dname= '';
+              $curl = curl_init();
+
+              curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.line.me/v2/bot/profile/".$userID,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                  "authorization: Bearer oOuhFrVkrCn3ngWNcdA96wED/ZtTR3Y8xEozvIP0zdAfamJCNsuZZHDAByWu70/7U6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVVUpnDm09h8C0VmOEcKNsi9RHTFNbBv5V5EA3FaugRewdB04t89/1O/w1cDnyilFU=",
+                  "cache-control: no-cache",
+                  "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
+                ),
+              ));
+
+              $response = curl_exec($curl);
+              $err = curl_error($curl);
+
+              curl_close($curl);
+
+              if ($err) {
+                echo "cURL Error #:" . $err;
+              } else {
+
+              $data = json_decode($response,true);
+              $username =  $data['displayName'];
+              }
+
               $vid = $itemx['id'];
               $balance = $itemx['balance_tks_balance'];
               $bet = $itemx['cf_956'];
