460f2d787d086948ac5403ef3a8f4ae6d8dd4a73
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-26 18:54:40 +0700

    1234

--- fbot/fbot.php
+++ fbot/fbot.php
@@ -168,36 +168,36 @@
           foreach($data["result"] as $item) { //foreach element in $arr
             $userID = $item['balance_tks_userid'];
             $username= '';
-            // $curl = curl_init();
-            //
-            //         curl_setopt_array($curl, array(
-            //           CURLOPT_URL => "https://api.line.me/v2/bot/group/Ccab41b434c5bffb33de6b56baa03f642/member/".$userID,
-            //           CURLOPT_RETURNTRANSFER => true,
-            //           CURLOPT_ENCODING => "",
-            //           CURLOPT_MAXREDIRS => 10,
-            //           CURLOPT_TIMEOUT => 30,
-            //           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            //           CURLOPT_CUSTOMREQUEST => "GET",
-            //           CURLOPT_HTTPHEADER => array(
-            //             "authorization: Bearer QyHSaarki7OaukcmDqWBZJD88fJb5N4evyOobmL7QyJOPpfV9YQz+gDgIvGXVXAEU6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVFq5PVGR/AHd5Ze80zm5YFBcjYGRUDqMHIDs9qSaLzLQdB04t89/1O/w1cDnyilFU=",
-            //             "cache-control: no-cache",
-            //             "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
-            //           ),
-            //         ));
-            //
-            //         $response = curl_exec($curl);
-            //         $err = curl_error($curl);
-            //
-            //         curl_close($curl);
-            //
-            //         if ($err) {
-            //           echo "cURL Error #:" . $err;
-            //         } else {
-            //
-            //         $data = json_decode($response,true);
-            //         $username =  $data['displayName'];
-            //         }
-            //
+            $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                      CURLOPT_URL => "https://api.line.me/v2/bot/group/Ccab41b434c5bffb33de6b56baa03f642/member/".$userID,
+                      CURLOPT_RETURNTRANSFER => true,
+                      CURLOPT_ENCODING => "",
+                      CURLOPT_MAXREDIRS => 10,
+                      CURLOPT_TIMEOUT => 30,
+                      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                      CURLOPT_CUSTOMREQUEST => "GET",
+                      CURLOPT_HTTPHEADER => array(
+                        "authorization: Bearer QyHSaarki7OaukcmDqWBZJD88fJb5N4evyOobmL7QyJOPpfV9YQz+gDgIvGXVXAEU6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVFq5PVGR/AHd5Ze80zm5YFBcjYGRUDqMHIDs9qSaLzLQdB04t89/1O/w1cDnyilFU=",
+                        "cache-control: no-cache",
+                        "postman-token: 6dc09c6b-dd83-81ca-75ed-71ce43b5edd7"
+                      ),
+                    ));
+
+                    $response = curl_exec($curl);
+                    $err = curl_error($curl);
+
+                    curl_close($curl);
+
+                    if ($err) {
+                      echo "cURL Error #:" . $err;
+                    } else {
+
+                    $data = json_decode($response,true);
+                    $username =  $data['displayName'];
+                    }
+
 
 
                     ?>
