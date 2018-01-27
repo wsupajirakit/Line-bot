0c011e4eaccdbc5f3a49fd15b4b436c83705319b
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-27 16:09:09 +0700

    1234

--- fbot/ibalance.php
+++ fbot/ibalance.php
@@ -53,7 +53,7 @@ if ($err) {
       $messages = [
         'type' => 'text',
         // 'text' => 'แทงผู้เล่น'.$player.'จำนวน'.$money.'ชื่อผู้เล่น'.$username.'ยอดคงเหลือ'.$balance.'vid:'.$vid
-        'text' => 'เติมเงินให้ '.$iname.' จำนวน'.$sum
+        'text' => 'เติมเงินให้ '.$iname.' จำนวน'.$newbalance.' รวม'.$sum
       ];
 
 
