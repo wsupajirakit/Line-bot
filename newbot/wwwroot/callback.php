641a2ff9f21b4d1c4b70a9ab0cd393527104cb25
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-27 19:49:51 +0700

    1234

--- newbot/wwwroot/callback.php
+++ newbot/wwwroot/callback.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('./line-bot.php');
+include ('../line-bot.php');
 
 $channelSecret = '56c7d7a131821a15a2f406ce300ea4eb';
 $access_token ='QyHSaarki7OaukcmDqWBZJD88fJb5N4evyOobmL7QyJOPpfV9YQz+gDgIvGXVXAEU6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVFq5PVGR/AHd5Ze80zm5YFBcjYGRUDqMHIDs9qSaLzLQdB04t89/1O/w1cDnyilFU=';
