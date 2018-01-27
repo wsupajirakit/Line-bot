9819863da2da2d539d3291898fc1eae40b116360
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-27 16:46:41 +0700

    sdas

---
+++ fbot/test.php
@@ -0,0 +1,16 @@
+<?php
+
+    $alltext = "1234";
+
+    if(substr_count($alltext,1)>1){
+      echo "fuck";
+    }
+    if(substr_count($alltext,2)>1){
+      echo "fuck";
+    }
+    if(substr_count($alltext,3)>1){
+      echo "fuck";
+    }
+    if(substr_count($alltext,4)>1){
+      echo "fuck";
+    }
