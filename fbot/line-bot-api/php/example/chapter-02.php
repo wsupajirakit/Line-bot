960c0bd9a550f6ec8b8146a5c408a84e45746aee
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-27 15:01:27 +0700

    2345

--- fbot/line-bot-api/php/example/chapter-02.php
+++ fbot/line-bot-api/php/example/chapter-02.php
@@ -2,12 +2,13 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4........3cff0';
-$access_token  = '2og9ogezC..... W5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '56c7d7a131821a15a2f406ce300ea4eb';
+$access_token  = 'QyHSaarki7OaukcmDqWBZJD88fJb5N4evyOobmL7QyJOPpfV9YQz+gDgIvGXVXAEU6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVFq5PVGR/AHd5Ze80zm5YFBcjYGRUDqMHIDs9qSaLzLQdB04t89/1O/w1cDnyilFU=';
+
 
 $bot = new BOT_API($channelSecret, $access_token);
-	
-$bot->sendMessageNew('U39f72cc.....d460d6ddf', 'Hello World !!');
+
+$bot->sendMessageNew('Ccab41b434c5bffb33de6b56baa03f642', 'Hello World !!');
 
 if ($bot->isSuccess()) {
     echo 'Succeeded!';
@@ -15,5 +16,5 @@ if ($bot->isSuccess()) {
 }
 
 // Failed
-echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
 exit();
