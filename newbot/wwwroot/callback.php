d30aa0aefb86a747af235ec4bd9084066d4fb148
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-01-27 19:46:34 +0700

    1234

--- newbot/wwwroot/callback.php
+++ newbot/wwwroot/callback.php
@@ -1,25 +1,24 @@
-<?php // callback.php
-define("LINE_MESSAGING_API_CHANNEL_SECRET", '56c7d7a131821a15a2f406ce300ea4eb');
-define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'QyHSaarki7OaukcmDqWBZJD88fJb5N4evyOobmL7QyJOPpfV9YQz+gDgIvGXVXAEU6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVFq5PVGR/AHd5Ze80zm5YFBcjYGRUDqMHIDs9qSaLzLQdB04t89/1O/w1cDnyilFU=');
+<?php
 
-require __DIR__."/../vendor/autoload.php";
+include ('./line-bot.php');
 
-$bot = new \LINE\LINEBot(
-    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
-    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
-);
+$channelSecret = '56c7d7a131821a15a2f406ce300ea4eb';
+$access_token ='QyHSaarki7OaukcmDqWBZJD88fJb5N4evyOobmL7QyJOPpfV9YQz+gDgIvGXVXAEU6ouir3bOeDcpShjwTOJib4P6jWHYh31pVMM2CAwUeVFq5PVGR/AHd5Ze80zm5YFBcjYGRUDqMHIDs9qSaLzLQdB04t89/1O/w1cDnyilFU=';
 
-$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
 
-$events = $bot->parseEventRequest($body, $signature);
+$bot = new BOT_API($channelSecret, $access_token);
 
-foreach ($events as $event) {
-        $reply_token = $event->getReplyToken();
-        // $text = $event->getText();
-        // $bot->replyText($reply_token, $text);
+if (!empty($bot->isEvents)) {
 
-        $bot->replyText($reply_token, json_encode($bot->message));
+	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
+
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
+	exit();
+
 }
-
-echo "OK";
