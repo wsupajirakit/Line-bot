c7175bd09ed655e350ad03b28063e0cd262a8ebd
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-02-05 12:02:45 +0700

    1234

--- botbackendfive.php
+++ botbackendfive.php
@@ -2456,11 +2456,33 @@ Issue",
                     if ($err) {
                       echo "cURL Error #:" . $err;
                     } else {
-                      echo $response;
+                      $messagex = [
+                        'type' => 'text',
+                        // 'text' => 'แทงผู้เล่น'.$player.'จำนวน'.$money.'ชื่อผู้เล่น'.$username.'ยอดคงเหลือ'.$balance.'vid:'.$vid
+                        'text' => 'ทดสอบโย่วๆ'
+                      ];
+
+
+                      $url = 'https://api.line.me/v2/bot/message/push';
+                      $data = [
+                        'to' => $groupID,
+                        'messages' => [$messagex],
+                      ];
+                      $post = json_encode($data);
+                      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+                      $ch = curl_init($url);
+                      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                      $result = curl_exec($ch);
+                      curl_close($ch);
                     }
 
 
-                    $messages = [
+                    $messagex = [
                       'type' => 'text',
                       'text' => json_decode('"\uD83D\uDE00"').'ปิดรอบที่ # '.$cround.json_decode('"\u2705"')
                     ];
