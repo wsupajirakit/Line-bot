e8d575393d1220a2bbdd051df0f2f719f69b10e2
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-02-06 16:42:34 +0700

    2134

--- botbackendone.php
+++ botbackendone.php
@@ -77,6 +77,7 @@ if (!is_null($events['events'])) {
         $player= strtoupper(strstr($text, '-', true));
         $money  = substr($text, (strpos($text, '-') ?: -1) + 1);
         $money = substr($money,0,3);
+        $money = preg_replace('/[^0-9]/', '', $money);
         $moneylen = strlen($money);
         $ix= '';
         $tx= '';
