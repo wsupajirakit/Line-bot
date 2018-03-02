66a669ccf9a6d0d2008aac27cba85bbcc4aa569a
wsupajirakit <wsupajirakit@wsupajirakitMBP.local>
2018-03-02 13:00:10 +0700

    2134

--- botbackend12.php
+++ botbackend12.php
@@ -2693,6 +2693,26 @@ if($countcheck==1){
 
 
 
+          $url = 'https://api.line.me/v2/bot/message/reply';
+    			$data = [
+    				'replyToken' => $replyToken,
+    				'messages' => [$messages],
+    			];
+    			$post = json_encode($data);
+    			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+    			$ch = curl_init($url);
+    			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    			$result = curl_exec($ch);
+    			curl_close($ch);
+
+    			echo $result . "\r\n";
+
+
 
     }
     else if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
